1682848980afbb6ca047fbd8e71b43e2ed7ed13f
RaffaeleCaravetta <raffaelecaravetta13@gmail.com>
2023-10-12 16:22:43 +0200

    esercizio 7

--- src/main/java/raffaelecaravetta/Application.java
+++ src/main/java/raffaelecaravetta/Application.java
@@ -14,6 +14,7 @@ public class Application {
 
         File file = new File("src/output.txt");
         List<Product> products = new ArrayList<>();
+        final ArrayList<String>[] contentArray = new ArrayList[]{new ArrayList<>()};
         for(int i =0;i<=100;i++){
             Product product = new Product("NomeProdotto"+i,"CategoriaProdotto"+i,"PrezzoProdotto"+i);
             products.add(product);
@@ -25,12 +26,32 @@ public class Application {
                     try {
                         FileUtils.writeStringToFile(file, product.getName()+"@"+product.getCategory()+"@"+product.getPrice()
                             + "#", StandardCharsets.UTF_8, true);
+
+
+                        String contenuto = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+
+                        leggiProdottiDaDisco(contentArray[0],contenuto);
+
+                        contentArray[0].forEach(System.out::println);
+
+
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 }
 
             );
+
+
+    }
+
+
+    private static void leggiProdottiDaDisco(ArrayList arrayList,String contenuto){
+        String[] parts = contenuto.split("#");
+
+        for(int i =0 ;i<=99;i++){
+            arrayList.add(parts[i]);
+        }
 
 
     }
